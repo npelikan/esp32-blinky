2d876b768413988b8a4cb21c5899723d88b18102
Nick Pelikan <nick.pelikan@posit.co>
2024-11-13 18:10:55 -0700

    IT BLINKS

---
+++ esp32-blinky/src/main.rs
@@ -0,0 +1,18 @@
+use esp_idf_hal::{delay::FreeRtos, gpio::PinDriver, peripherals::Peripherals};
+use esp_idf_sys as _;
+
+fn main() {
+    esp_idf_sys::link_patches();
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut led_pin = PinDriver::output(peripherals.pins.gpio2).unwrap();
+
+    loop {
+        led_pin.set_low().unwrap();
+        FreeRtos::delay_ms(1000);
+
+        led_pin.set_high().unwrap();
+        FreeRtos::delay_ms(1000);
+        println!("blink");
+    }
+}
